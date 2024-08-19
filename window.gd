abdffdf8849c37d9ba39a824442523ec459d225a
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2024-08-18 17:50:55 -0700

    WIP (1)

--- window.gd
+++ window.gd
@@ -8,15 +8,17 @@ signal pre_close_anim
 ## and just before queue_free.
 signal pre_destroy
 
-@export var _content: CanvasItem
+## The content of the window. [Control]s set to fill the space work best.
+@export var content: CanvasItem
 
-@onready var _close: Button = %Close
+@onready var _close := %Close as Button
 @onready var _close_clicked := _close.pressed
-@onready var _display_borders: NinePatchRect = $DisplayBorders
-@onready var _animations: AnimationPlayer = $Animations
-@onready var _content_slot: Control = %ContentSlot
-@onready var _timer_display: TextureProgressBar = %TimerDisplay
-@onready var _timer: Timer = $Timer
+@onready var _display_borders := $DisplayBorders as NinePatchRect
+@onready var _animations := $Animations as AnimationPlayer
+@onready var _content_slot := %ContentSlot as Control
+@onready var _negative_timer := %NegativeTimerDisplay as TextureProgressBar
+@onready var _positive_timer := %PositiveTimerDisplay as TextureProgressBar
+@onready var _timer := $Timer as Timer
 
 func _refresh_has_close_button(value: bool) -> void:
 	if _close != null:
@@ -28,12 +30,18 @@ func _refresh_has_close_button(value: bool) -> void:
 		has_close_button = value
 		_refresh_has_close_button(value)
 
-func _refresh_has_timer(value: bool) -> void:
-	if _timer_display != null:
-		_timer_display.visible = value
-	if _timer != null:
-		if not value:
-			pass
+func _refresh_timer_mode(value: String) -> void:
+	if _negative_timer == null or _positive_timer == null:
+		return
+	var is_neg := value == "negative"
+	var is_pos := value == "positive"
+	_negative_timer.visible = is_neg
+	_positive_timer.visible = is_pos
+
+@export_enum("none", "negative", "positive") var timer_mode: String = "none":
+	set(value):
+		timer_mode = value
+		_refresh_timer_mode(value)
 
 ## Used internally in the close animation to center the pivot on the ninepatch.
 func auto_center_pivot() -> void:
@@ -43,13 +51,30 @@ func _contain(content: Control) -> void:
 	content.reparent(_content_slot)
 	content.position = Vector2.ZERO
 
+## Close and destroy this window immediately, firing pre_destroy.
+func close_destroy() -> void:
+	pre_destroy.emit()
+	queue_free()
+
+var _borders: Vector2i
+
+var inner_size: Vector2:
+	get:
+		return size - Vector2(_borders)
+
 func _ready() -> void:
-	if _content != null:
-		if not _content.visible:
-			_content.visible = true
-		_contain.call_deferred(_content)
+	if content != null:
+		if not content.visible:
+			content.visible = true
+		_contain.call_deferred(content)
 	else:
 		print("no content smh")
+	
+	_borders = Vector2i(
+		_display_borders.patch_margin_left + _display_borders.patch_margin_right,
+		_display_borders.patch_margin_top + _display_borders.patch_margin_bottom
+	)
+	
 	_refresh_has_close_button(has_close_button)
 	pre_close_anim.connect(auto_center_pivot)
 	_close_clicked.connect(func():
@@ -61,8 +86,3 @@ func _ready() -> void:
 	)
 	auto_center_pivot()
 	_animations.play("open")
-
-## Close and destroy this window immediately, firing pre_destroy.
-func close_destroy() -> void:
-	pre_destroy.emit()
-	queue_free()
